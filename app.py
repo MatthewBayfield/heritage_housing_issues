51bf074ed8a16169c88254d64273e37e58250e33
MatthewBayfield <matthew.bayfield@yahoo.com>
2023-01-29 22:00:56 +0000

    Create sale price study multipage app page for the dashboard

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 from app_pages.multipage import MultiPage
 from app_pages.page_summary import page_summary_body
+from app_pages.sale_price_study import sale_price_study_body
 
 app = MultiPage(app_name= "Heritage Housing Issues")
 
 app.add_page('Project Summary', page_summary_body)
+app.add_page("How are a house's attributes related to its sale price", sale_price_study_body)
 
 app.run()
---
+++ app_pages/sale_price_study.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def sale_price_study_body():
+    pass
