26c961b75d0bcf28605616b5f379e78b1326ccf5
MatthewBayfield <matthew.bayfield@yahoo.com>
2023-01-29 23:20:38 +0000

    Add Overview section to sale price study dashboard page

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ from app_pages.sale_price_study import sale_price_study_body
 app = MultiPage(app_name= "Heritage Housing Issues")
 
 app.add_page('Project Summary', page_summary_body)
-app.add_page("How are a house's attributes related to its sale price", sale_price_study_body)
+app.add_page('Sale price correlation study', sale_price_study_body)
 
 app.run()
--- app_pages/sale_price_study.py
+++ app_pages/sale_price_study.py
@@ -1,4 +1,16 @@
 import streamlit as st
 
 def sale_price_study_body():
-    pass
+    st.write("## The relationship between a house's attributes and its sale price")
+    st.write('### Overview')
+    st.write(f'In the dataset there were 22 individual attributes, often related, and groupable by being a measure of size, quality, age,\n'
+             f'condition or form.\n'
+             f'Despite the large number of attributes, the actual number of independent features is far less, due to there being many moderate-to-strong\n'
+             f'correlations between attributes.\n\n'
+
+             f'Some possible limitations of the dataset worth noting, are the lack of any attributes relating to its location,\n'
+             f'or the time of sale, both which likely impact the sale price.\n\n'
+
+             f'Irrespective of any dataset limitations, statistically significant correlations/relationships were discovered between various house attributes\n'
+             f'and the sale price, which shall now be explored.'
+             )
