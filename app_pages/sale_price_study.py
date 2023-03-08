4287b02556d1048af7ac270cc571943020e46716
MatthewBayfield <matthew.bayfield@yahoo.com>
2023-03-08 12:53:05 +0000

    Add checkbox input element to toggle house attribute table display
    On the sale_price_study app page, selecting the checkbox reveals
    the house attribute metadata table

--- app_pages/sale_price_study.py
+++ app_pages/sale_price_study.py
@@ -42,33 +42,36 @@ def sale_price_study_body():
              f'and the sale price, which shall now be explored.\n')
 
     st.write('### The house attributes')
-    st.write(f'Here is a table of the house attributes in the dataset, their meanings, abbreviations, and possible values:\n\n')
-    st.markdown(f'''|Variable|Meaning|Values|
-                    |:----|:----|:----|
-                    |1stFlrSF|First Floor square feet|334 - 4692|
-                    |2ndFlrSF|Second-floor square feet|0 - 2065|
-                    |BedroomAbvGr|Bedrooms above grade (does NOT include basement bedrooms)|0 - 8|
-                    |BsmtExposure|Refers to walkout or garden level walls|Gd: Good Exposure; Av: Average Exposure; Mn: Minimum Exposure; No: No Exposure; None: No Basement|
-                    |BsmtFinType1|Rating of basement finished area|GLQ: Good Living Quarters; ALQ: Average Living Quarters; BLQ: Below Average Living Quarters; Rec: Average Rec Room; LwQ: Low Quality; Unf: Unfinshed; None: No Basement|
-                    |BsmtFinSF1|Type 1 finished square feet|0 - 5644|
-                    |BsmtUnfSF|Unfinished square feet of basement area|0 - 2336|
-                    |TotalBsmtSF|Total square feet of basement area|0 - 6110|
-                    |GarageArea|Size of garage in square feet|0 - 1418|
-                    |GarageFinish|Interior finish of the garage|Fin: Finished; RFn: Rough Finished; Unf: Unfinished; None: No Garage|
-                    |GarageYrBlt|Year garage was built|1900 - 2010|
-                    |GrLivArea|Above grade (ground) living area square feet|334 - 5642|
-                    |KitchenQual|Kitchen quality|Ex: Excellent; Gd: Good; TA: Typical/Average; Fa: Fair; Po: Poor|
-                    |LotArea| Lot size in square feet|1300 - 215245|
-                    |LotFrontage| Linear feet of street connected to property|21 - 313|
-                    |MasVnrArea|Masonry veneer area in square feet|0 - 1600|
-                    |EnclosedPorchSF|Enclosed porch area in square feet|0 - 286|
-                    |OpenPorchSF|Open porch area in square feet|0 - 547|
-                    |OverallCond|Rates the overall condition of the house|10: Very Excellent; 9: Excellent; 8: Very Good; 7: Good; 6: Above Average; 5: Average; 4: Below Average; 3: Fair; 2: Poor; 1: Very Poor|
-                    |OverallQual|Rates the overall material and finish of the house|10: Very Excellent; 9: Excellent; 8: Very Good; 7: Good; 6: Above Average; 5: Average; 4: Below Average; 3: Fair; 2: Poor; 1: Very Poor|
-                    |WoodDeckSF|Wood deck area in square feet|0 - 736|
-                    |YearBuilt|Original construction date|1872 - 2010|
-                    |YearRemodAdd|Remodel date (same as construction date if no remodelling or additions)|1950 - 2010|
-                    |SalePrice|Sale Price|34900 - 755000|''')
+    view_attributes = st.checkbox('Select to view a table of the house attributes')
+
+    if view_attributes:
+        st.write(f'Here is a table of the house attributes in the dataset, their meanings, abbreviations, and possible values:\n\n')
+        st.markdown(f'''|Variable|Meaning|Values|
+                        |:----|:----|:----|
+                        |1stFlrSF|First Floor square feet|334 - 4692|
+                        |2ndFlrSF|Second-floor square feet|0 - 2065|
+                        |BedroomAbvGr|Bedrooms above grade (does NOT include basement bedrooms)|0 - 8|
+                        |BsmtExposure|Refers to walkout or garden level walls|Gd: Good Exposure; Av: Average Exposure; Mn: Minimum Exposure; No: No Exposure; None: No Basement|
+                        |BsmtFinType1|Rating of basement finished area|GLQ: Good Living Quarters; ALQ: Average Living Quarters; BLQ: Below Average Living Quarters; Rec: Average Rec Room; LwQ: Low Quality; Unf: Unfinshed; None: No Basement|
+                        |BsmtFinSF1|Type 1 finished square feet|0 - 5644|
+                        |BsmtUnfSF|Unfinished square feet of basement area|0 - 2336|
+                        |TotalBsmtSF|Total square feet of basement area|0 - 6110|
+                        |GarageArea|Size of garage in square feet|0 - 1418|
+                        |GarageFinish|Interior finish of the garage|Fin: Finished; RFn: Rough Finished; Unf: Unfinished; None: No Garage|
+                        |GarageYrBlt|Year garage was built|1900 - 2010|
+                        |GrLivArea|Above grade (ground) living area square feet|334 - 5642|
+                        |KitchenQual|Kitchen quality|Ex: Excellent; Gd: Good; TA: Typical/Average; Fa: Fair; Po: Poor|
+                        |LotArea| Lot size in square feet|1300 - 215245|
+                        |LotFrontage| Linear feet of street connected to property|21 - 313|
+                        |MasVnrArea|Masonry veneer area in square feet|0 - 1600|
+                        |EnclosedPorchSF|Enclosed porch area in square feet|0 - 286|
+                        |OpenPorchSF|Open porch area in square feet|0 - 547|
+                        |OverallCond|Rates the overall condition of the house|10: Very Excellent; 9: Excellent; 8: Very Good; 7: Good; 6: Above Average; 5: Average; 4: Below Average; 3: Fair; 2: Poor; 1: Very Poor|
+                        |OverallQual|Rates the overall material and finish of the house|10: Very Excellent; 9: Excellent; 8: Very Good; 7: Good; 6: Above Average; 5: Average; 4: Below Average; 3: Fair; 2: Poor; 1: Very Poor|
+                        |WoodDeckSF|Wood deck area in square feet|0 - 736|
+                        |YearBuilt|Original construction date|1872 - 2010|
+                        |YearRemodAdd|Remodel date (same as construction date if no remodelling or additions)|1950 - 2010|
+                        |SalePrice|Sale Price|34900 - 755000|''')
                     
     st.write('### Correlation tests')
     st.info(f'Two types of correlation tests that calculate correlation coefficients that range from 0 to 1, and indicate the strength of the correlation,\n'
