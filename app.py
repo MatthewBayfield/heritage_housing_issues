044d9f6fde9ceea987b9f564f07d2a39399f749d
MatthewBayfield <matthew.bayfield@yahoo.com>
2023-03-08 15:06:49 +0000

    Create project_hypotheses app page. Add title and main section headings

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import streamlit as st
 from app_pages.multipage import MultiPage
 from app_pages.page_summary import page_summary_body
 from app_pages.sale_price_study import sale_price_study_body
+from app_pages.project_hypotheses_page import project_hypotheses_body
 
 app = MultiPage(app_name= "Heritage Housing Issues")
 
 app.add_page('Project Summary', page_summary_body)
 app.add_page('Sale price correlation study', sale_price_study_body)
+app.add_page('Project Hypotheses', project_hypotheses_body)
 
 app.run()
