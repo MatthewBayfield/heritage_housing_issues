72ac8687c9d0bbf059255d0f2cfe686075da0918
MatthewBayfield <matthew.bayfield@yahoo.com>
2023-03-09 05:36:42 +0000

    Add load_pkl_file function to data_management.py

--- src/data_management.py
+++ src/data_management.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import pandas as pd
+import joblib
 
 @st.cache
 def load_csv(filepath):
     df = pd.read_csv(filepath)
     return df
+
+@st.cache
+def load_pkl_file(file_path):
+    return joblib.load(filename=file_path)
