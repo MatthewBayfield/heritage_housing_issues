d50c55b7edd32c6286743d7211967982caead2ec
MatthewBayfield <matthew.bayfield@yahoo.com>
2023-04-11 14:04:34 +0000

    allow_output_mutation for cached functions

--- src/data_management.py
+++ src/data_management.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-@st.cache
-def load_csv(filepath):
-    df = pd.read_csv(filepath)
+@st.cache(allow_output_mutation=True)
+def load_csv(filepath, **kwargs):
+    df = pd.read_csv(filepath, **kwargs)
     return df
 
-@st.cache
-def load_pkl_file(file_path):
-    return joblib.load(filename=file_path)
+@st.cache(allow_output_mutation=True)
+def load_pkl_file(file_path, **kwargs):
+    return joblib.load(filename=file_path, **kwargs)
